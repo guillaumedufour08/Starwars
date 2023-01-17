48f725c76a7422f5599d300079049aa7be25e481
Guillaume Dufour <guillaume.c.dufour@desjardins.com>
2023-01-17 16:12:02 -0500

    [SW-002] - Création d'un utilitaire afin de trouver l'id d'un entité

--- app/src/main/java/com/example/starwars/StringManager.kt
+++ app/src/main/java/com/example/starwars/StringManager.kt
@@ -1,14 +1,19 @@
 package com.example.starwars
 
-import android.app.Application
 import android.view.View
 
-class StringFinder {
+class StringManager {
     companion object {
         fun findFromName(root: View, name: String): String {
             val packageName = root.context.packageName
             val resourceId = root.resources.getIdentifier(name, "string", packageName)
             return if (resourceId != 0)  root.context.getString(resourceId) else name
+        }
+
+        fun retriveIdFromURL(url: String) : String {
+            val trimedURL = url.trimEnd('/')
+            val start = trimedURL.lastIndexOf("/") + 1
+            return url.substring(start, trimedURL.lastIndex + 1)
         }
     }
 }
