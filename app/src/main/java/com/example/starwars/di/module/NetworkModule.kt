051458d7627c7073bddd4539c807e5b9f690fb5f
Guillaume Dufour <guillaume.c.dufour@desjardins.com>
2023-01-19 15:18:23 -0500

    Suppression d'un import de « moshi »

--- app/src/main/java/com/example/starwars/di/module/NetworkModule.kt
+++ app/src/main/java/com/example/starwars/di/module/NetworkModule.kt
@@ -9,9 +9,7 @@ import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
 import okhttp3.OkHttpClient
 import retrofit2.Retrofit
-import retrofit2.converter.gson.GsonConverterFactory
 import retrofit2.converter.jackson.JacksonConverterFactory
-import retrofit2.converter.moshi.MoshiConverterFactory
 import java.util.concurrent.TimeUnit
 import javax.inject.Singleton
 
